3ace57c79fd5951b3ae95de19124e1f9200f9492
Ravi Lamichhane <ravilamichhane30@gmail.com>
2020-09-03 17:50:03 +0545

    why choose us update

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,17 +16,18 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "Dee Why Cleaning Services",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || ""
+        content:
+          "Dee Why Cleaning Services is a cleaning service provider based on dee why australia . We provide qualit cleaning services to our clients "
       }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+
     link: [
       {
         href:
--- pages/index.vue
+++ pages/index.vue
@@ -21,16 +21,15 @@
             <v-col>
               <h2>Why Choose Us</h2>
 
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat itaque consequuntur mollitia inventore minima earum neque molestiae laboriosam, optio voluptates facilis esse officiis est ullam fugiat quisquam voluptate accusantium aliquam!</p>
+              <p class="text-why-us">
+                Dee Why Cleaning Services is dedicated to helping you create a clean environment in your business. We have many years of experience in the industry and our highly skilled team of cleaners provide professional cleaning services so that you don’t have to do it yourself.
+                Our skilled cleaning agents are here for you seven days a week whenever required. We can tailor our cleaning services to suite your individual requirements. If you have a cleaning emergency, we can arrange help within 2–24 hours. The major reasons to choose our service are :
+              </p>
 
-              <p
-                class="text-why-us"
-              >Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti tenetur est accusamus quasi beatae, autem vel repudiandae. Earum quod aperiam nemo minima libero magnam ea quae a laborum, ratione expedita!</p>
-
-              <li>service Lorem ipsum dolor sit amet.</li>
-              <li>service Lorem ipsum dolor sit amet.</li>
-              <li>service Lorem ipsum dolor sit amet.</li>
-              <li>service Lorem ipsum dolor sit amet.</li>
+              <li>High standard cleaning service.</li>
+              <li>Open communicarion to CLients</li>
+              <li>Cost-effective cleaning solutions</li>
+              <li>Skilled and reliable staffs</li>
             </v-col>
           </v-row>
         </v-col>
@@ -43,10 +42,12 @@
             <v-col>
               <h2>What we offer to our Clients</h2>
 
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt fugiat aliquam ea, perferendis voluptatem sequi error omnis ipsa at laboriosam, corporis earum corrupti et optio dignissimos doloribus consequatur, sit suscipit.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, dolores nihil, tempore autem enim ducimus, error minima nobis deserunt quod reprehenderit corporis perspiciatis suscipit labore facilis quae aspernatur porro ullam.
-              </p>
+              <p>We provide our clients with a comprehensive cleaning service. We pride ourselves on networking with industry suppliers to ensure we are up-to-date with tailored cleaning services to fit with our clients’ cleaning requirements with the latest trends and cleaning products. We offer sound, environmentally friendly products that fit your cleaning budget. We care about you and our environmet</p>
+
+              <li>Corporate & Social Responsibility</li>
+              <li>Quality Assurance & Checks</li>
+              <li>Environmental Awareness</li>
+              <li>Recycling Importance</li>
             </v-col>
           </v-row>
         </v-col>
